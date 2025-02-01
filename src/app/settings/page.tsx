7e9eaa0ce4dcca4aafd924484cb984d80f8a1952
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-02-01 03:49:24 +0100

    small changes

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,17 +1,11 @@
 "use client";
 
-import { Switch } from "@/components/ui/switch";
-import { Label } from "@radix-ui/react-label";
 import { ModeToggle } from "@/components/ModeToggle";
 
 const Settings = () => {
   return (
     <div className="flex flex-col items-center">
       <h1>Settings</h1>
-      {/* <div className="flex align-center gap-3"> */}
-      {/*   <Label>Theme</Label> */}
-      {/*   <Switch /> */}
-      {/* </div> */}
       <ModeToggle />
     </div>
   );
--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -35,7 +35,7 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>Flashy</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (

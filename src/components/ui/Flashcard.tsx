906310a2eb21cc5d8e4cac808e61327b9d861ae7
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-02-06 18:11:36 +0100

    flip flashcard

--- src/components/ui/Flashcard.tsx
+++ src/components/ui/Flashcard.tsx
@@ -1,13 +1,23 @@
 import { Card } from "./card";
+import { useState } from "react";
 
 interface FlashcardProps {
     term: string;
     definition: string;
 }
 
+
+
 const Flashcard = ({term, definition}: FlashcardProps) => {
+    const [flipped, setFlipped] = useState(false);
+
+    const handleClick = () => {
+        setFlipped(!flipped);
+    }
     return (
-        <Card className="w-3/4 h-48 flex items-center justify-center">{term}</Card>
+        <Card className="w-3/4 h-48 flex items-center justify-center" onClick={handleClick}>
+            {flipped ? definition : term}
+        </Card>
     );
 };
 

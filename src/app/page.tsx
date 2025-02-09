4d5b69e246d1bba73751068a3942d9e45a6b8341
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-02-09 03:37:50 +0100

    fetch sets in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,50 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
-import { signOut, useSession } from "next-auth/react";
+import { Label } from "@/components/ui/label";
+import { useState, useEffect } from "react";
 
 
 export default function Home() {
   const router = useRouter();
+  const [sets, setSets] = useState([]);
+
+  useEffect(() => {
+    const fetchSets = async () => {
+      try {
+        const response = await fetch(`/api/sets`, {
+          method: "GET",
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setSets(data.sets);
+        } else {
+          console.error("Failed to fetch sets");
+        }
+        
+      } catch (error) {
+        console.error("Failed to fetch sets:", error); 
+      }
+    } 
+
+    fetchSets();
+  }, []);
+
 
   const handleNavigation = () => {
     router.push("/flashcards");
   };
 
-  const showSession = () => {
-    if (status === "authenticated") {
-      return (
-        <button
-          className="border border-solid border-black rounded"
-          onClick={() => {
-            signOut({ redirect: false }).then(() => {
-              router.push("/");
-            });
-          }}
-        >
-          Sign Out
-        </button>
-      )
-    } else if (status === "loading") {
-      return (
-        <span className="text-[#888] text-sm mt-7">Loading...</span>
-      )
-    } else {
-      return (
-        <Link
-          href="/login"
-          className="border border-solid border-black rounded"
-        >
-          Sign In
-        </Link>
-      )
-    }
-
   return (
     <div className="flex flex-col items-center">
-      <div className="flex flex-col items-center w-full max-w-xs">
-        <p className="mb-4 text-lg font-medium">Start</p>
-        <Button variant="outline" onClick={handleNavigation}>
-          Button
-        </Button>
+      <div>
+        <Label className="text-lg">Folders</Label>
+      </div>
+      <div>
+        <Label className="text-lg">Sets</Label>
       </div>
     </div>
   );
-  };
-}
+};
+
